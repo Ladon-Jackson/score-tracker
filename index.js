985e32c6ce65dc7825b99a7bf41531b3e7411a66
Domryuken <ladonjackson@hotmail.co.uk>
2022-12-27 21:18:35 +0000

    adding mongo

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// import express, {json, Application} from "express";
-import express from "express";
-import { Server, createServer } from "http";
+import express, {json} from "express";
+import {createServer } from "http";
 import cors from "cors";
+import { ObjectId, MongoClient, MongoError, Collection } from 'mongodb'
+import { url } from './ConfigStuff.js'
 
 const app = express();
 const server = createServer(app);
 const port = 6942;
 
-//app.use(json());
+const getCollection = () => MongoClient.connect(url).then(client => client.db("testdb").collection('testCollection'))
+
+app.use(json());
 app.use(cors());
+
+app.route("/this-works").get()
 
 server.listen(port, () => {
   console.log("Listening on port " + port)
 }); 
+
+function name() {
+
+}
